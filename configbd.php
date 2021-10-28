da9a4c8ba39b1de3de64a53048eb1987a172204e
Julio2DAW <jramosgago.guadalupe@alumnado.fundacionloyola.net>
2021-10-28 10:18:20 +0200

    Agregar la función visualizar, me da error cuando presiono mostrar, pendiente de corregir - 10:18 28/10/2021

--- configbd.php
+++ configbd.php
@@ -5,12 +5,15 @@
     @author Julio Antonio Ramos Gago <jramosgago.guadalupe@alumnado.fundacionloyola.net>
     @license GPL v3 2021
   */
+  
   /*
     define("HOSTNAME", "2daw.esvirgua.com");
     define("USERNAME", "user2daw_15");
     define("PW", "%WLB-?8~8U2C");
     define("DB", "user2daw_BD1-15");
   */
+
+  //Probar el progrma en local
   define("HOSTNAME", "localhost");
   define("USERNAME", "root");
   define("PW", "");
--- visualizar.php
+++ visualizar.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <!--Julio Antonio Ramos Gago-->
+<?php
+  require 'configbd.php'; //Acceso a la base de datos.
+  $conectar = new mysqli(HOSTNAME, USERNAME, PW, DB);
+?>
 <html lang="es">
   <head>
     <meta charset="utf-8" />
@@ -21,7 +25,33 @@
         <p>Menú Vertical</p>
       </aside>
       <article>
-        <p>gfhasgfkgsig</p>
+        <form action="consultar.php" method="post">
+          <label for="dni">DNI</label><br />
+          <input type="text" name="dni" id="dni"/><br />
+          <select name="filtrado" id="filtrado">
+            <option value="ASC">Nombre (A-Z)</option>
+            <option value="DESC">Nombre (Z-A)</option>
+          </select><br />
+          <input type="submit" value="Mostrar"><br /><!--Búsqueda-->
+        </form>
+        <!--Volver a la página de inicio-->
+        <a href="index.html">Volver</a>
+        <?php
+          //Busqueda de empleados según su filtrado
+          if($_POST){
+            if(trim($_POST['dni'])=="")
+              $consulta="SELECT * FROM empleados ORDER BY nombre ".$_POST['filtrado'].";";
+            else
+              $consulta="SELECT * FROM empleados WHERE dni LIKE '".$_POST['dni']."%' ORDER BY nombre ".$_POST['filtrado'].";";
+            $resultado=$conectar->query($consulta);
+            echo '<table>';
+            while($fila=$resultado->fetch_assoc()){
+              echo '<p> DNI: '.$fila['dni'].' -> Nombre: '.$fila['nombre'].'</p>';
+              echo '<p><a href="borrar.php?id='.$fila['idEmpleado'].'">Borrar</a></p>';//Opción de borrar empleado
+              echo '<p><a href="modificar.php?id='.$fila['idEmpleado'].'">Modificar</a></p>';//Opción de modificar empleado
+            }
+          }
+        ?>
       </article>
     </main>
     <footer>
