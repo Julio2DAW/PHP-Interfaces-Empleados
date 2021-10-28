e3051d77ca55cc329c5387a103ef8a408eef9c00
Julio2DAW <jramosgago.guadalupe@alumnado.fundacionloyola.net>
2021-10-28 09:46:08 +0200

    Cambio en el config para poder probar el programa en local, y agregada la función dar de alta. Funciona - 9:45 28/10/2021

--- alta.php
+++ alta.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <!--Julio Antonio Ramos Gago-->
+<?php
+  require 'configbd.php'; //Acceso a la base de datos.
+  $conectar = new mysqli(HOSTNAME, USERNAME, PW, DB);
+?>
 <html lang="es">
   <head>
     <meta charset="utf-8" />
@@ -21,7 +25,30 @@
         <p>Menú Vertical</p>
       </aside>
       <article>
-        <p>gfhasgfkgsig</p>
+        <form action="alta.php" method="post">
+          <label for="dni">DNI</label><br/>
+          <input type="text" name="dni" id="dni"/><br/>
+          <label for="nombre">Nombre Completo</label><br/>
+          <input type="text" name="nombre" id="nombre"/><br/>
+          <label for="correo">Correo Electrónico</label><br/>
+          <input type="text" name="correo" id="correo"/><br/>
+          <label for="telefono">Teléfono Personal</label><br/>
+          <input type="text" name="telefono" id="telefono"/><br/>
+          <input type="submit" value="Agregar"/><!--Agregar empleado con una *consulta (insert into).-->
+          <input type="reset" value="Borrar Datos"/><br/><!--Limpiar formulario si no queremos realizar la operación.-->
+        </form>
+        <!--Volver a la página de inicio-->
+        <a href="index.html">Volver</a>
+        <?php
+          //Una vez envia los datos del formulario, se hará un insert into con esos datos.
+          //Si estos datos son aceptados mostrará un *mensaje por pantalla de confirmación.
+          if($_POST){
+            //*consulta
+            $consulta="INSERT INTO Empleados (dni, nombre, correo, telefono) VALUES ('".$_POST['dni']."', '".ucwords($_POST['nombre'])."', '".$_POST['correo']."', '".$_POST['telefono']."');";
+            $resultado=$conectar->query($consulta);
+            echo '<p>Empleado agregado correctamente</p>';//*mensaje
+          }
+        ?>
       </article>
     </main>
     <footer>
